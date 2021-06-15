797e0dcaf3f5c537002fa57ef4b91147c7fe8358
rohannj1993 <rohannj1993@gmail.com>
2021-06-15 18:54:14 -0400

    made first set of questions and had prompts appear

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github account",
+        message: "What is your github name?",
+      },
+      {
+        type: "input",
+        message: "What is this projects title?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is your description for this project?",
+        name: "description",
+      },
+
+      {
+          type: "input",
+          message: "How does a user use this project?",
+          name: "usage"
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {inquirer.prompt(questions).then((answer) =>  {
+    console.log(answer)
+})}
 
 // Function call to initialize app
 init();
